06c573706c789f6e83534b7167488ae2a71bb5b1
Nando <nandogoulart.m@gmail.com>
2024-04-16 10:40:13 -0300

    feat() ajuste css

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -112,7 +112,7 @@ export default function Page({ token }: { token: string }) {
                                                     <p className="text-gray-900 whitespace-no-wrap">{new Date(task.prazo).toISOString().split('T')[0]}</p>
                                                 </td>
                                                 <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                                    <div className="flex justify-around">
+                                                    <div className="flex justify-center gap-3">
                                                         <button
                                                             onClick={() => handleEditTask(task)}
                                                             className="bg-indigo-600 px-4 py-2 rounded-md text-white font-semibold tracking-wide cursor-pointer"
